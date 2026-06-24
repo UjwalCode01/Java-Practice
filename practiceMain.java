da0711f115abfbd3b19906b85ce9d04439e961ab
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-06-24 19:30:25 +0530

    practice

---
+++ practiceMain.java
@@ -0,0 +1,8 @@
+public class practiceMain {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 93;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
