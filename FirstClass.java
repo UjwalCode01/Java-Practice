d76963298089d7ad8f36de73ee4a166dec6b1b89
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-06-24 19:02:39 +0530

    first class.java

--- FirstClass.java
+++ FirstClass.java
@@ -1,7 +1,43 @@
 class FirstClass {
-    public static void main(String args[]){
-        System.out.println("hello world");
-        System.out.println("i am ujwal");
+    public static void main(String[] args) {
+        System.out.print("hello world");
+        System.out.print("i am ujwal vijay daivikar"); 
+        System.out.println("i live in yavatmal");
+        System.out.println("i study in 3rd yrs");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*\n**");
+        //variables
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        System.out.println(sum);
+        int mul = a * b;
+        System.out.println(mul);
+        int x = 10;
+        int y = 5;
+        int ans = x * y / x - y;
+        System.out.println(ans);
+        int ans1 = (x * y)/(x - y);
+        System.out.println(ans1); 
     }
 }
+
+// scanner 
+ 
+ import java.util.*;
+
+ class Firstclass {
+     public static void main(String[]args){
+     //input
+    Scanner Sc = new Scanner(System.in);
+    String name = Sc.nextLine();
+    System.out.println(name);
+     }
+   
+ }
+
+
 
