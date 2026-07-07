4a0b99c5da2dac0169f7373aa1519224ee777114
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-07-07 07:57:55 +0530

    condition class if else

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        System.out.println("hello world");
+        String name = "tony stark";
+        int age = 48;
+        double price = 25.26;
+        int a = 25;
+        int b = 10;
+        name = "iron man";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(a + b); 
+        int s = 29;
+        int n = 39;
+        int sum = s + n;
+        System.out.println(sum);
     }
 }
-
