f5fa3406f292c3f07320008b5b91a4c82ffc83ea
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-07-09 08:03:19 +0530

    while loop

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[]args){
         //for loop ===
@@ -21,7 +23,18 @@ public class Main {
         //System.out.println("i am ujwal");
       //}while(i < 11);
     
-      
+      Scanner sc = new Scanner (System.in);
+      int n = sc.nextInt();
+      int sum = 0;
+      for(int i = 0 ; i <= n; i++){
+        sum = sum + i;
+         
+      }
+    
+      System.out.println(sum);
+       
+
+
 
         
         
