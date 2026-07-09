b5515dd714bb86cfdf3078843452d7ebd2a58da3
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-07-09 07:38:02 +0530

    lec 4 code loop  counter

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[]args){
-        
-        
+        //counter++ => counter = counter + 1
+        for(int counter = 0; counter <11; counter ++){
+            System.out.println(counter);
+            
+        }
     }
 
 }
