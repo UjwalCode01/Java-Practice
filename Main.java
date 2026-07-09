431e64b866c8deb4e07e13592ebe665921ea4e2d
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-07-09 07:24:54 +0530

    lec 4 code

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[]args){
-        int a = 28;
-        int b = 98;
-        int sum = a + b;
-        System.out.println(sum);
+        
         
     }
 
