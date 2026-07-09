b707099b0b203f09d77f453bee50c4e22564bc4a
UjwalCode01 <ujwaldaivikar@gmail.com>
2026-07-09 07:54:13 +0530

    while loop

--- Main.java
+++ Main.java
@@ -1,10 +1,30 @@
 public class Main {
     public static void main(String[]args){
+        //for loop ===
         //counter++ => counter = counter + 1
-        for(int counter = 0; counter <11; counter ++){
-            System.out.println(counter);
-            
-        }
+    //for(int i = 0; i < 29; i++){
+       // System.out.println(i);
+   // }
+       //int == starting , i<29 ==loop ,i++ update condition
+       //for(int counter = 0; counter <3; counter = counter +1){
+       // System.out.println("hello world");
+     //do loop ===
+      // }
+      //  int i = 0;
+        //do {
+            //System.out.println(i);
+           // i = i + 1;
+
+      //  }  while(i < 12);
+      //int i = 12;
+      //do {
+        //System.out.println("i am ujwal");
+      //}while(i < 11);
+    
+      
+
+        
+        
     }
 
 }
